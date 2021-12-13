aef44f47ef7ade9451b86dad6cfb51f1e53c22b8
Biswajit <biswajit.majumdar@carnationit.com>
2021-12-13 14:51:26 +0530

    only Welcome.ejs html page added

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require("express")
 
+const expressLayout = require('express-ejs-layouts') // for ui
+
 const app = express();
 
-const PORT = process.env.PORT || 5000
+//EJS
+app.use(expressLayout)
+app.set('view engine','ejs')
+
+//
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
 
 //Router
 app.use('/',require('./routes/index'))
 app.use('/users',require('./routes/users'))
+
+const PORT = process.env.PORT || 5000
 
 
 
@@ -15,3 +25,6 @@ app.use('/users',require('./routes/users'))
 app.listen(PORT,()=>{
     console.log(`Server started at port no ${PORT}`)
 })
+
+
+//ejs for ui
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ const express = require("express")
 const router = express.Router()
 
 router.get('/',(req,res)=>{
-    res.send('Welcome')
+    //res.send('Welcome')
+    res.render('welcome.ejs')  // welcome.ejs
 })
 
 module.exports = router
