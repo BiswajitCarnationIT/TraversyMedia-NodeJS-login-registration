aa24ced896501fe3e53c02583fbccf7fb9f42f48
Biswajit <biswajit.majumdar@carnationit.com>
2021-12-13 20:04:09 +0530

    Validation messages also added

--- routes/users.js
+++ routes/users.js
@@ -18,8 +18,37 @@ router.get('/register',(req,res)=>{
 
 // Register Handel
 router.post('/register',(req,res)=>{
-    console.log(req.body)
-    res.send('hello')
+    // console.log(req.body)
+    // res.send('hello')
+    const { name,email,password,password2 } = req.body;
+    let errors = [];
+
+    // Check required fields
+    if(!name || !email || !password || !password2){
+        errors.push({msg:'Please fill in all fields'})
+    }
+
+    // Check password match
+    if(password !== password2){
+        errors.push({msg:'Passwords do not'})
+    }
+
+    // Check pass length
+    if(password.length < 6){
+        errors.push({msg:'password should be atleast 6 character'})
+    }
+
+    if(errors.length > 0){
+        res.render('register',{
+            errors,
+            name,
+            email,
+            password,
+            password2
+        })  
+    }else{
+        res.send('pass')
+    }
 })
 
 module.exports = router
